635ddaa5a2fc753923de9639992766e85b16d921
Alexandra <moya.sawa@gmail.com>
2018-02-24 20:55:54 +0200

    Module2-task1 - ESLint errors fixed

--- js/stat.js
+++ js/stat.js
@@ -14,12 +14,12 @@ var FONT = '16px PT Mono';
 var USER_Y = CLOUD_Y + CLOUD_PADDING + (TEXT_HEIGHT + FONT_GAP) * 2 + TEXT_HEIGHT;
 var BAR_MAX_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -29,18 +29,17 @@ var getMaxElement = function(arr) {
   }
 
   return maxElement;
-}
+};
 
-var getRandomNum = function(min, max) {
+var getRandomNum = function (min, max) {
   return Math.random() * (max - min + 1) + min;
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  console.log(times);
   renderCloud(ctx, CLOUD_X + SHADOW_SHIFT, CLOUD_Y + SHADOW_SHIFT, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  ctx.fillStyle = "#000";
+  ctx.fillStyle = '#000';
   ctx.font = FONT;
   ctx.fillText('Ура вы победили!', CLOUD_X + CLOUD_PADDING, CLOUD_Y + CLOUD_PADDING);
   ctx.fillText('Список результатов:', CLOUD_X + CLOUD_PADDING, CLOUD_Y + CLOUD_PADDING + TEXT_HEIGHT + FONT_GAP);
@@ -54,17 +53,16 @@ window.renderStatistics = function (ctx, names, times) {
   for (var j = 0; j < names.length; j++) {
     barHeight = times[j] * BAR_MAX_HEIGHT / maxTime;
     barY = (BAR_MAX_HEIGHT - barHeight) + CLOUD_Y + CLOUD_PADDING + (TEXT_HEIGHT + FONT_GAP) * 3 + FONT_GAP;
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillText(names[j], CLOUD_X + CLOUD_PADDING + (BAR_WIDTH + GAP) * j, USER_Y);
 
     if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       randomNum = Math.floor(getRandomNum(0, 255));
-      console.log(randomNum);
       ctx.fillStyle = 'rgba(0, 0, ' + randomNum + ', 1)';
     }
-    ctx.fillRect(CLOUD_X + CLOUD_PADDING + (BAR_WIDTH + GAP) * j, barY , BAR_WIDTH, barHeight);
+    ctx.fillRect(CLOUD_X + CLOUD_PADDING + (BAR_WIDTH + GAP) * j, barY, BAR_WIDTH, barHeight);
   }
 
 };
